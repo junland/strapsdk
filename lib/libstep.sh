3a22e8598476004889b1a324f3afc12c25223cf3
junland <unlandj2012@gmail.com>
2021-09-20 15:27:44 -0500

    Fix stuff.

--- lib/libstep.sh
+++ lib/libstep.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 step_msg() {
   printf " >>>> %s\n" "$1"
+}
+
+step_msg_red() {
+  printf "\e[31m >>>> %s \n\e[0m" "$1"
+}
+
+step_msg_green() {
+  printf "\e[32m >>>> %s \n\e[0m" "$1"
 }
 
 step_display_strap_env() {
@@ -10,4 +18,9 @@ step_display_strap_env() {
   local env=$(env | grep "STRAP" | sort | sed 's/=/ set as: /g')
 
   echo "$env"
+}
+
+step_fail() {
+  step_msg_red "Failed: $1"
+  exit 1
 }
--- lib/libstrap.sh
+++ lib/libstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 strap_msg() {
   printf " >>>> %s\n" "$1"
